7bbcdc5c70e284a185ee64bf0deb34998f2dd881
Ludovic T <thuard.ludovic@gmail.com>
2022-05-06 10:17:10 +0200

    Contact form implemented

--- js/contact.js
+++ js/contact.js
@@ -1,42 +1,22 @@
-//Récuperation des informations du formulaire
-function getFormData(){
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let childName = document.getElementById("childName").value;
-    let childAge = document.getElementById("childAge").value;
-    let message = document.getElementById("message").value;
-
-    const formData = {
-        name: name,
-        email: email,
-        childName: childName,
-        childAge: childAge,
-        message: message
-    };
-
-    return formData;
+//Initialising email API
+function init(){
+    emailjs.init('TwLYgRBPXw8xH5uel')
 };
+init();
 
-//Envoi des données au serveur
-function sendFormData(){
-    const formData = getFormData();
-    const sendData ={
-        method: 'POST',
-        body: JSON.stringify(formData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-    const url  = 'http://localhost:3000/contact';
-    fetch(url, sendData)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        if(data.status === 'success'){
-            alert('Votre message a bien été envoyé');
-        }
-        else{
-            alert('Une erreur est survenue');
-        }
-    })
+//Sending email
+function sendEmail(){
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('service_58t85si', 'template_wa35t9r', this)
+            .then(function() {
+                console.log('SUCCESS!');
+                alert("Message envoyé !");
+                window.location.reload();
+            }, function(error) {
+                console.log('FAILED...', error);
+                alert("Erreur lors de l'envoi du message !");
+            });
+    });
 };
+sendEmail();
