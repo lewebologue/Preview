e2af726c7b7955ea8686dbce36b512727d7932cc
Ludovic T <thuard.ludovic@gmail.com>
2022-05-13 09:59:31 +0200

    Scroll top bug fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//JQuery functionality
+
 (function ($) {
     "use strict";
 
@@ -24,21 +26,6 @@
             $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
         }
     });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
 
     // Header carousel
     $(".header-carousel").owlCarousel({
@@ -79,3 +66,8 @@
     
 })(jQuery);
 
+//JavaScript functionality
+
+function scrollToTop(){
+    document.getElementById("scrollToTop").scrollTo({behavior: "smooth"});
+}
